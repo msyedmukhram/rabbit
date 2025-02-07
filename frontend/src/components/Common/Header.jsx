123f74e9a83460600e1ec775c68b58f339f428c7
Syed-Mukhram-Ullah MUJEEB <syed-mukhram-ullah.mujeeb@rntbci.com>
2025-02-07 11:08:04 +0530

    navbar in header completed

--- frontend/src/components/Common/Header.jsx
+++ frontend/src/components/Common/Header.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import Topbar from '../Layout/Topbar'
+import Navbar from './Navbar'
 
 const Header = () => {
   return (
-    <div>
+    <header className='border-b border-gray-200'>
         {/* Topbar */}
-        <Topbar></Topbar>
+        <Topbar />
         {/* Navbar */}
+        <Navbar />
         {/* Cart Drawer */}
 
-    </div>
+    </header>
   )
 }
 
