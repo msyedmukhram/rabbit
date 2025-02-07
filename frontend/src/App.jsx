41e2192433a0cea7185c0d542cc002a7b6c9e888
Syed-Mukhram-Ullah MUJEEB <syed-mukhram-ullah.mujeeb@rntbci.com>
2025-02-07 23:26:39 +0530

    assets added and one section in homepage completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import UserLayout from './components/Layout/UserLayout'
+import Home from './pages/Home'
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<UserLayout />}>
+          {/* Home Page */}
+          <Route index element={<Home />} />
         </Route>
         <Route>
           {/* Admin Layout */}
--- frontend/src/components/Layout/UserLayout.jsx
+++ frontend/src/components/Layout/UserLayout.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import Header from '../Common/Header'
 import Footer from '../Common/Footer'
+import { Outlet } from 'react-router-dom'
 
 const UserLayout = () => {
   return (
     <>
         {/* Header */}
         <Header></Header>
+       
         {/* Main Content */}
+        <main>
+            <Outlet />
+        </main>
+       
         {/* Footer */}
         <Footer />
     </>
