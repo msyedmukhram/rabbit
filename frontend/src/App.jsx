be45cad391256003bd1f68b29a7bb42e9c2a1db5
Syed-Mukhram-Ullah MUJEEB <syed-mukhram-ullah.mujeeb@rntbci.com>
2025-02-07 09:48:50 +0530

    bug fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={UserLayout}>
+        <Route path='/' element={<UserLayout />}>
         </Route>
         <Route>
           {/* Admin Layout */}
