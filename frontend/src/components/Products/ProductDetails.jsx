b6f33e9d5fa300e485f4c4d864063582ae4d1c91
Syed-Mukhram-Ullah MUJEEB <syed-mukhram-ullah.mujeeb@rntbci.com>
2025-02-10 19:30:33 +0530

    Best seller completed

--- frontend/src/components/Products/ProductDetails.jsx
+++ frontend/src/components/Products/ProductDetails.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { toast } from "sonner";
+import ProductGrid from "./ProductGrid";
 
 const selectedProduct = {
   name: "Stylish Jacket",
@@ -21,6 +22,50 @@ const selectedProduct = {
     },
   ],
 };
+
+const similarProducts = [
+  {
+    _id: 1,
+    name: 'Product 1',
+    price: 100,
+    images: [
+      {
+        url: 'https://picsum.photos/500/500?random=1'
+      } 
+    ]
+  },
+  {
+    _id: 2,
+    name: 'Product 2',
+    price: 200,
+    images: [
+      {
+        url: 'https://picsum.photos/500/500?random=2'
+      } 
+    ]
+  },
+  {
+    _id: 3,
+    name: 'Product 3',
+    price: 300,
+    images: [
+      {
+        url: 'https://picsum.photos/500/500?random=3'
+      } 
+    ]
+  },
+  {
+    _id: 4,
+    name: 'Product 4',
+    price: 400,
+    images: [
+      {
+        url: 'https://picsum.photos/500/500?random=4'
+      } 
+    ]
+  },
+  
+]
 
 const ProductDetails = () => {
   const [mainImage, setMainImage] = useState('');
@@ -178,6 +223,13 @@ const ProductDetails = () => {
               </table>
             </div>
           </div>
+        </div>
+      
+        <div className="mt-20">
+          <h2 className="text-2xl text-center font-medium mb-4">
+            You May Also Like
+          </h2>
+          <ProductGrid products={similarProducts} />
         </div>
       </div>
     </div>
